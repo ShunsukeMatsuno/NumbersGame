e2d486796939299b30eb932be488da9ddbfa2fd9
Shunsuke Matsuno <44533758+ShunsukeMatsuno@users.noreply.github.com>
2020-06-12 19:54:01 +0900

    Parallelized the computation of P

--- R/compute_P.R
+++ R/compute_P.R
@@ -1,13 +1,29 @@
-compute_P <- function(theta, S = 1000){
+compute_P <- function(theta, S = 1000, parallel = TRUE){
   e_vec <- -20:20    # each state (bin)
   result <- array(dim = c(S, length(e_vec)))   # S x 41  array
   
   # compute m_opt for each e (b)
-  for(i in seq_along(e_vec)){
-    result_temp <- compute_optimal_manipulation_for_e(e_vec[i], theta, S)
-    result[,i] <- result_temp$R        
+  if(parallel == TRUE){
+    # parallel computation
+    
+    ## register
+    library(doParallel)
+    cores = parallel::detectCores(logical = FALSE)
+    doParallel::registerDoParallel(cores)
+    
+    result <- foreach (i = seq_along(e_vec),
+                    .packages = c('dplyr', 'purrr', 'NumbersGame','tidyr'),
+                    .combine = 'cbind') %dopar% {
+                      compute_optimal_manipulation_for_e(e_vec[i], theta, S)$R
+                    }
+  }else{
+    # sequential computation
+    for(i in seq_along(e_vec)){
+      result_temp <- compute_optimal_manipulation_for_e(e_vec[i], theta, S)
+      result[,i] <- result_temp$R        
+    }
   }
-  
+
   # compute P
   P <- array(dim = c(41, 41))
   
